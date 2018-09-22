361a1bafc99cf3e281fd39c80c6da607dab36a50
shavyg2 <shavyg2@gmail.com>
2018-09-22 12:10:45 -0400

    sigin

--- digital_signage/user/signin.js
+++ digital_signage/user/signin.js
@@ -1,19 +1,39 @@
 let AWS = require('aws-sdk');
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
-
 exports.handler = function (event, context, callback) {
-    let {username} = event;
+    const AmazonCognitoIdentity = new AWS.CognitoIdentity(cognito_idp);
+    
+    let { username, password } = event;
 
-    cognito_idp.adminGetUser({
-        UserPoolId: process.env.user_pool_id, /* required */
-        Username: `${username}` /* required */
-    }, function (error, user) {
-        if (error) {
-            throw error;
-        }else{
-            console.log(user)
-            callback(null, 'Successfully executed');
-        }
+    var authenticationData = {
+        Username: username,
+        Password: password,
+    };
+
+    var authenticationDetails = new AWS.AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+    var poolData = {
+        UserPoolId: process.env.user_pool_id,
+        ClientId: process.env.app_id
+    };
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+    var userData = {
+        Username: username,
+        Pool: userPool
+    };
+    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function (result) {
+            var accessToken = result.getAccessToken().getJwtToken();
+
+            /* Use the idToken for Logins Map when Federating User Pools with identity pools or when passing through an Authorization Header to an API Gateway Authorizer */
+            var idToken = result.idToken.jwtToken;
+            callback(null, idToken)
+        },
+
+        onFailure: function (err) {
+            callback(err)
+        },
+
     });
 
 }
